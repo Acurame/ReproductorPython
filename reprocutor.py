b8bf36dd92f2c533983eb68bbc7ebb3f5fcad318
Acurame <acuramee@gmail.com>
2022-04-24 17:43:28 -0600

    Principio del reproductor

---
+++ reprocutor.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+raiz=Tk()
+raiz.title("Reproductor")
+ventana = Frame()
+
+ventana.pack(fill="both",expand="True")
+
+ventana.config(bg="black")
+ventana.config(width="650",height="350")
+
+raiz.mainloop()
