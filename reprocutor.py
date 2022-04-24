a4d55f51a9ce9e16f0fa5fca3703d772e39d6a70
Acurame <acuramee@gmail.com>
2022-04-24 16:52:08 -0600

    algunas modificasiones

--- reprocutor.py
+++ reprocutor.py
@@ -1,10 +1,10 @@
+from struct import pack
 from tkinter import *
 import numpy as np
 import sounddevice as sd
 import soundfile as sf
 
-
-
+volumenM = 1
 
 raiz=Tk()
 raiz.title("Reproductor")
@@ -15,14 +15,23 @@ ventana.pack(fill="both",expand="True")
 ventana.config(bg="black")
 ventana.config(width="650",height="350")
 
-def Sonido():
+def Sonido(volumen):
     x,fs = sf.read('acdc.wav',dtype='float32')
+
+    y = volumen * x
     muestras = 1*fs 
     zeros = np.zeros( muestras-1, dtype=int)
-    sd.play(x,fs)
+    sd.play(y,fs)
     status = sd.wait()
 
-Reproducir = Button(ventana, text="Reproducir" , command=lambda:Sonido())
-Reproducir.grid(row=1 , column= 0)
+def Volumen_Menos():
+    v = volumenM - 0.1
+    return v
+
+VolumenMenos = Button(raiz, text="-" , command=lambda:Volumen_Menos())
+VolumenMenos.pack()
+
+Reproducir = Button(ventana, text="Reproducir" , command=lambda:Sonido(volumenM))
+Reproducir.grid(row=0 , column= 1)
 
 raiz.mainloop()
